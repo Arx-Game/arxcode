7f09f93f457e64c8c9f22fee8f7fe40e00ab9596
dvoraen <42825596+dvoraen@users.noreply.github.com>
2020-06-02 10:58:00 -0700

    Fixing my Roll code break.

--- commands/base_commands/rolling.py
+++ commands/base_commands/rolling.py
@@ -69,6 +69,7 @@ class CmdDiceCheck(ArxCommand):
     def func(self):
         """Run the @check command"""
         caller = self.caller
+        retainer = None
         is_retainer = "retainer" in self.switches
         flub = "flub" in self.switches
 
@@ -90,7 +91,6 @@ class CmdDiceCheck(ArxCommand):
             if is_retainer:
                 args, retainer_id = self._extract_retainer_id(args)
                 #TODO: get retainer object here
-                retainer = None
 
             args, difficulty = self._extract_difficulty(args)
             stat, skill = self._extract_stat_skill(args)
@@ -98,7 +98,7 @@ class CmdDiceCheck(ArxCommand):
             caller.msg(str(err))
             return
 
-        stats_and_skills.do_dice_check(caller, stat=stat, skill=skill, difficulty=difficulty, quiet=quiet, flub=flub)
+        stats_and_skills.do_dice_check(caller, retainer=retainer, stat=stat, skill=skill, difficulty=difficulty, quiet=quiet, flub=flub)
         
         if quiet:
             self._send_quiet_roll_msg()
