5cb9fd8a447731112aa6669d93f7dd67374207dd
Tehom <surly.mime@gmail.com>
2022-10-30 18:59:39 -0400

    Fix path in command

--- web/admintools/management/commands/backup_db.py
+++ web/admintools/management/commands/backup_db.py
@@ -1,10 +1,14 @@
 from django.core.management.base import BaseCommand
+from django.conf import settings
 import subprocess
 
 
 class Command(BaseCommand):
     """
     This implements backing up our database.
+
+    Usage:
+        evennia backup_db
     """
     default_name = "evennia_backup.db3"
 
@@ -12,7 +16,11 @@ class Command(BaseCommand):
         parser.add_argument('--db_name', type=str)
 
     def handle(self, *args, **options):
-        pass
+        if options["db_name"]:
+            backup_name = options["db_name"]
+        else:
+            backup_name = self.default_name
+        self.create_sqlite_backup(backup_name)
 
     def create_sqlite_backup(self, backup_name=""):
         """Runs sqlite3 backup command.
@@ -26,7 +34,7 @@ class Command(BaseCommand):
         """
         backup_name = backup_name or self.default_name
         sqlite_cmd = f".backup '{backup_name}'"
-        db_name = "evennia.db3"
+        db_name = settings.DATABASES["default"]["NAME"]
         self.stdout.write(f"Copying database {db_name} to {backup_name}.")
-        subprocess.run(["sqlite3", db_name, f'"{sqlite_cmd}"'], shell=True)
+        subprocess.run(["sqlite3", db_name, f'{sqlite_cmd}'], shell=True)
         self.stdout.write("Copy complete.")
